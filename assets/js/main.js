fc74938287f8d7f0c52402f66d431b443174bb23
Siddharth Tayde <taydesiddharth999@gmail.com>
2026-04-06 21:32:43 +0530

    Add day 1 on this project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 // ================= Show and close menu ======================= //
 const navMenu = document.getElementById('nav_menu'),
-     navToggle = document.getElementById('nav_toggle'),
-     navClose = document.getElementById('nav_close');
+    navToggle = document.getElementById('nav_toggle'),
+    navClose = document.getElementById('nav_close');
 
-if(navToggle){
+if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.className += ' ';
         //the .show class in written in the input.css file in utilities layer
@@ -11,7 +11,7 @@ if(navToggle){
     })
 }
 
-if(navClose){
+if (navClose) {
     navClose.addEventListener('click', () => {
         let className = navMenu.className;
         if (className.includes("show")) {
@@ -27,10 +27,22 @@ const navlinks = document.querySelectorAll('.nav_link');
 const linkAction = () => {
     const navMenu = document.getElementById('nav_menu');
     let className = navMenu.className;
-        if (className.includes("show")) {
-            className = className.replace("show", '')
-            navMenu.className = className;
-        }
-} 
+    if (className.includes("show")) {
+        className = className.replace("show", '')
+        navMenu.className = className;
+    }
+}
 
-navlinks.forEach((n) => {n.addEventListener('click', linkAction)})
+navlinks.forEach((n) => { n.addEventListener('click', linkAction) })
+
+// ================= change header style  ======================= //
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // Add the .scroll-header class if the bottom scroll of the vie
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header')
+    }else{
+        header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader)
