6b0762bad62399cd356f6b1b0d50fec615159f81
Siddharth Tayde <taydesiddharth999@gmail.com>
2026-04-06 17:11:28 +0530

    Add - responsive navbar completed

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// ================= Show and close menu ======================= //
+const navMenu = document.getElementById('nav_menu'),
+     navToggle = document.getElementById('nav_toggle'),
+     navClose = document.getElementById('nav_close');
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.className += ' ';
+        //the .show class in written in the input.css file in utilities layer
+        navMenu.classList.toggle('show');
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        let className = navMenu.className;
+        if (className.includes("show")) {
+            className = className.replace("show", '')
+            navMenu.className = className;
+        }
+    })
+}
+
+// ================= close mobile menu ======================= //
+const navlinks = document.querySelectorAll('.nav_link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav_menu');
+    let className = navMenu.className;
+        if (className.includes("show")) {
+            className = className.replace("show", '')
+            navMenu.className = className;
+        }
+} 
+
+navlinks.forEach((n) => {n.addEventListener('click', linkAction)})
